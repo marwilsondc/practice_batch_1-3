824d4924c792cf36f46dc4f1a4212fed14d629b2
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 21:57:06 +0800

    added code for batch 1 program 4

--- programs_batch_1/print_product_two_numbers.py
+++ programs_batch_1/print_product_two_numbers.py
@@ -1,4 +1,6 @@
 #ask user to input 2 numbers
-
+first_number = int(input("input first number = "))
+second_number = int(input("input second number = "))
 
 #print product of 2 numbers
+print(first_number * second_number)
