3ee4ea29a7118a2675ff61ff9b8f909ac90ce10c
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 00:31:14 +0800

    added code for batch 2 program 5

--- programs_batch_2/print_remainder_two_numbers.py
+++ programs_batch_2/print_remainder_two_numbers.py
@@ -1,2 +1,6 @@
 #ask user to input 2 numbers 
+first_number = int(input("input first_number = "))
+second_number = int(input("input second number = "))
+
 #print result of remainder
+print(first_number % second_number)
