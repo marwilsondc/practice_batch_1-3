30b8616e9ae28617a32d69770b0df1a53f682cf4
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 01:06:36 +0800

    added pseudocode for batch 2 program 7

---
+++ programs_batch_2/print_how_many_even_numbers.py
@@ -0,0 +1,5 @@
+#initialize number list and even list
+#create while loop to ask user to input 10 numbers
+#add inputs to number list
+#evaluate even numbers and add to even list
+#print results by counting even numbers through len()
