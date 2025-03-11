854254213d258d74dfeed08589f2f888703bcdb4
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 00:57:11 +0800

    added code for batch 2 program 6

--- programs_batch_2/print_result_first_number_minus_all.py
+++ programs_batch_2/print_result_first_number_minus_all.py
@@ -1,3 +1,9 @@
 #initialize number list
+number_list = list()
+
 #create while loop to ask user to input 10 numbers
+while len(number_list) < 10:
+    number_list.append(int(input("input a number = ")))
+
 #print result by indexing the first number and subtracting the sum of the rest
+print(number_list[0] - sum(number_list[1:10]))
