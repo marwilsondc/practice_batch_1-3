4b1b9fe7e6f88d315a236bca4b4eaef39bb9099c
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 21:32:14 +0800

    added pseudocode and finished print_bigger_number.py

---
+++ programs_batch_1/print_bigger_number.py
@@ -0,0 +1,9 @@
+#ask user to input 2 numbers
+first_number = int(input("input first number = "))
+second_number = int(input("input second number = "))
+
+if first_number > second_number:
+    print(first_number)
+
+else:
+    print(second_number)
