677fc626893a3b4598da216432c5f9a4151ac11c
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 23:25:16 +0800

    added code for batch 2 program 3

--- programs_batch_2/print_difference_two_numbers.py
+++ programs_batch_2/print_difference_two_numbers.py
@@ -1,2 +1,6 @@
 #ask user to input 2 numbers
+first_number = int(input("input first number = "))
+second_number = int(input("input second number = "))
+
 #print difference of 2 numbers
+print(first_number - second_number)
