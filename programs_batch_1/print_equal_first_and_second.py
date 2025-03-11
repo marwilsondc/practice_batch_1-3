95c547447eac28aa5484ef5f097938585318fcce
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 21:43:49 +0800

    added code

--- programs_batch_1/print_equal_first_and_second.py
+++ programs_batch_1/print_equal_first_and_second.py
@@ -1,2 +1,7 @@
 #ask user to input 2 numbers
+first_number = int(input("input first number = "))
+second_number = int(input("input second number = "))
+
 #create if loop to evaluate if inputs are equal
+if first_number == second_number:
+    print("equal")
