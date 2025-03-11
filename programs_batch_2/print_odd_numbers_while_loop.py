f4e26c2ee4be352cf1584a38a9f2c7b90899798a
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 01:48:25 +0800

    added pseudocode and code for batch 2 program 8

---
+++ programs_batch_2/print_odd_numbers_while_loop.py
@@ -0,0 +1,12 @@
+#initialize number count for while loop
+number_count = 0
+
+#create while loop that stops at 100
+while number_count < 100:
+    
+    #evaluate odd numbers and print if odd
+    if number_count % 2 != 0:
+        print(number_count)
+    
+    #add 1 to number count
+    number_count += 1
