38a00875e90a4ccc4d72a3d0dd7844df4a780d47
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 22:17:01 +0800

    added code for batch 1 program 7

--- programs_batch_1/print_sum_ten_numbers.py
+++ programs_batch_1/print_sum_ten_numbers.py
@@ -1,3 +1,9 @@
 #initialize a list to store 10 number inputs
+number_list = list()
+
 #create while loop to append 10 numbers
+while len(number_list) < 10:
+    number_list.append(int(input("input a number = ")))
+
 #print sum of 10 number list
+print(sum(number_list))
