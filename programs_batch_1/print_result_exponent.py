42beee7e19d8279ac7ba3e4f017306177d7581f7
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 22:08:38 +0800

    added code for batch 1 program 6

--- programs_batch_1/print_result_exponent.py
+++ programs_batch_1/print_result_exponent.py
@@ -1,3 +1,6 @@
 #ask user to input 2 numbers
+first_number = int(input("input base = "))
+second_number = int(input("input exponent = "))
 
 #raise first number to second number
+print(first_number**second_number)
