9eb3165ca7562ccf8db9f831f11e2dac367d22aa
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 22:25:19 +0800

    added pseudocode to batch 1 program 8

---
+++ programs_batch_1/print_how_many_odd_numbers.py
@@ -0,0 +1,5 @@
+#initialize odd list and number list
+#create while loop to ask user to input 10 numbers
+#add input to number list
+#evaluate input and append odd numbers to odd list
+#return length of odd list
