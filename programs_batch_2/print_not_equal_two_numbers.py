67c9309c4d2c61c75c44407a2746409c9880b66a
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 23:17:53 +0800

    added code for batch 2 program 2

--- programs_batch_2/print_not_equal_two_numbers.py
+++ programs_batch_2/print_not_equal_two_numbers.py
@@ -1,3 +1,9 @@
 #ask user to input 2 numbers
+first_number = int(input("input first number = "))
+second_number = int(input("input second number = "))
+
 #create if statement to evaluate the 2 numbers
-#print "not equal" if the statement returns true
+if first_number != second_number:
+
+    #print "not equal" if the statement returns true
+    print("not equal")
