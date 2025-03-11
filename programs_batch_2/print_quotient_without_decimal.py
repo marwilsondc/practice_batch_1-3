59261de095fbcc7167493ed2b12ed9a23c6169cb
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 23:51:25 +0800

    added code for batch 2 program 4

--- programs_batch_2/print_quotient_without_decimal.py
+++ programs_batch_2/print_quotient_without_decimal.py
@@ -1,2 +1,6 @@
 #ask user to input 2 numbers
+first_number = int(input("input first number = "))
+second_number = int(input("input second number = "))
+
 #print quotient without decimal through int()
+print(int(first_number/second_number))
