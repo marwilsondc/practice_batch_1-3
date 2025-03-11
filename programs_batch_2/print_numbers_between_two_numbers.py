940c86d78b943182423d476634aa336be8850152
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 02:16:19 +0800

    modified pseudocode and added code for batch 2 program 10

--- programs_batch_2/print_numbers_between_two_numbers.py
+++ programs_batch_2/print_numbers_between_two_numbers.py
@@ -1,3 +1,10 @@
 #ask user to input 2 numbers
-#integrate the 2 inputs to range(), add 1 to second number
+first_number = int(input("input first number = "))
+second_number = int(input("input second number = "))
+
+#integrate the 2 inputs to range(), add 1 to first number
+number_range = range(first_number + 1, second_number)
+
 #create a for loop to iterate through range()
+for i in number_range:
+    print(i)
