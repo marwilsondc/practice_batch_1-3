20883cebd827db16e4eb45d43f7c075c1f0ab931
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 22:02:23 +0800

    added code for batch 1 program 5

--- programs_batch_1/print_quotient_with_decimal.py
+++ programs_batch_1/print_quotient_with_decimal.py
@@ -1,4 +1,6 @@
 #ask user to input 2 numbers
-
+first_number = int(input("input first number = "))
+second_number = int(input("input second number = "))
 
 #print quotient with decimal
+print(first_number/second_number)
