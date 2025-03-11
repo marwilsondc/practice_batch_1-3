c4b27683979fb961c20af11f04c5df668dc9bab0
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 01:58:24 +0800

    added code for batch 2 program 9

--- programs_batch_2/print_to_100_excluding_zero_and_five.py
+++ programs_batch_2/print_to_100_excluding_zero_and_five.py
@@ -1,3 +1,8 @@
 #create for loop with range 0-100
-#create if statement to evaluate numbers ending in zero or five within for loop
-#print i if if statement returns true
+for i in range(0,101):
+
+    #create if statement to evaluate numbers ending in zero or five within for loop
+    if i % 5 != 0:
+
+        #print i if if statement returns true
+        print(i)
