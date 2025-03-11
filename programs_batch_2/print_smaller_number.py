d875c10c23617f8b4723d944ee2d917056ded397
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 23:06:24 +0800

    added code for batch 2 program 1

--- programs_batch_2/print_smaller_number.py
+++ programs_batch_2/print_smaller_number.py
@@ -1,3 +1,12 @@
 #ask user to input 2 numbers
+first_number = int(input("input first number = "))
+second_number = int(input("input second number = "))
+
 #create if statement to evaluate smaller number
-#print smaller number
+if first_number < second_number:
+
+    #print smaller number
+    print(first_number)
+
+else:
+    print(second_number)
