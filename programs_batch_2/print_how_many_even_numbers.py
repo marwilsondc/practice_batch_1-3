3183b31065e4f8977407053be789118fa481aafe
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 01:14:31 +0800

    added code for batch 2 program 7

--- programs_batch_2/print_how_many_even_numbers.py
+++ programs_batch_2/print_how_many_even_numbers.py
@@ -1,5 +1,18 @@
 #initialize number list and even list
+number_list = list()
+even_list = list()
+
 #create while loop to ask user to input 10 numbers
-#add inputs to number list
-#evaluate even numbers and add to even list
+while len(number_list) < 10:
+    user_input = int(input("input a number = "))
+
+    #add inputs to number list
+    number_list.append(user_input)
+
+    #evaluate even numbers and add to even list
+    if user_input % 2 == 0:
+        even_list.append(user_input)
+
 #print results by counting even numbers through len()
+print(even_list)
+print(len(even_list))
