9f65f0750377a1a5d5e96fd74604073c6a2b4cee
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 21:52:26 +0800

    added code for batch 1 program 3

--- programs_batch_1/print_sum_of_two_numbers.py
+++ programs_batch_1/print_sum_of_two_numbers.py
@@ -1,4 +1,6 @@
 #ask user to input 2 numbers
-
+first_number = int(input("input first number = "))
+second_number = int(input("input second number = "))
 
 #print sum of two numbers
+print(first_number + second_number)
