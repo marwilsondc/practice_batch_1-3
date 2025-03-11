fd48a3fc2ca308738f3b3408a3f4d830fe210729
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-11 22:32:45 +0800

    added code batch 1 program 8

--- programs_batch_1/print_how_many_odd_numbers.py
+++ programs_batch_1/print_how_many_odd_numbers.py
@@ -1,5 +1,18 @@
 #initialize odd list and number list
+number_list = list()
+odd_list = list()
+
 #create while loop to ask user to input 10 numbers
-#add input to number list
-#evaluate input and append odd numbers to odd list
-#return length of odd list
+while len(number_list) < 10:
+    user_input = int(input("input a number = "))
+
+    #add input to number list
+    number_list.append(user_input)
+
+    #evaluate input and append odd numbers to odd list
+    if user_input % 2 != 0:
+        odd_list.append(user_input)
+
+#return odd list and length of odd list
+print(odd_list)
+print(len(odd_list))
