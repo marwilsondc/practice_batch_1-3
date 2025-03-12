59a54e1d23c12149d6670672103ba7b3a72bf915
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 18:08:24 +0800

    added pseudocode for batch 4 program 1

---
+++ programs_batch_4/display_all_duplicated_numbers.py
@@ -0,0 +1,5 @@
+#initialize number_list and display_list
+#create while loop to ask user for 10 inputs
+#append user_input to number_list 
+#evaluate inputs with duplicates and append user_input to display_list if true
+#display display_list
