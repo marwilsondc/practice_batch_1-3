80753b052ccd9c9b8caf4062aa9fce05e7870bfe
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 23:58:33 +0800

    added pseudocode for batch 4 program 3

---
+++ programs_batch_4/display_highest_number.py
@@ -0,0 +1,4 @@
+#initialize valid_flag, current_highest_number
+#create while loop to continue asking user for inputs
+#evaluate whether user_input is higher than current_highest_number
+#print current_highest_number
