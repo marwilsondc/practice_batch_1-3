d57d548492aee7cd12dbdb28ae35e9da7519162c
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-13 01:09:48 +0800

    added initial code for batch 4 program 5

--- programs_batch_4/display_average_of_all.py
+++ programs_batch_4/display_average_of_all.py
@@ -1,6 +1,20 @@
 #initialize valid_flag, number_list
+valid_flag = True
+number_list = list()
+
 #create while loop to keep asking user to input numbers
-#evaluate whether user_input is invalid (if statement + break) and set valid_flag to false if true
-#explicitly convert user_input to integer and store in convert_input
-#append convert_input into number_list
-#print sum of number_list and divide to length of number_list
+while valid_flag:
+    user_input = input("input a number = ")
+
+    #evaluate whether user_input is invalid (if statement + break) and set valid_flag to false if true
+    if user_input.isalpha() or user_input.isspace():
+        valid_flag = False
+
+    #explicitly convert user_input to integer and store in convert_input
+    convert_input = int(user_input)
+
+    #append convert_input into number_list
+    number_list.append(convert_input)
+
+    #print sum of number_list and divide to length of number_list
+    print(sum(number_list)/len(number_list))
