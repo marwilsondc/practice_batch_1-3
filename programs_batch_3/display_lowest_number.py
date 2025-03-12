510573d95e132f59ee0efc190e034a5d2c5137c6
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 17:11:22 +0800

    added pseudocode for batch 3 program 4

---
+++ programs_batch_3/display_lowest_number.py
@@ -0,0 +1,5 @@
+#initialize valid_flag and number_list
+#create while loop that continuously asks user for integer inputs
+#evaluate whether user_input is invalid (if statement) and set valid_flag to false if true
+#append user_input to number_list and use sort()
+#display lowest number with list indexing
