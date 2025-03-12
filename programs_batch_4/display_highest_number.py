6ef5392a4f6490dbd1a4368a7f479170992671f8
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-13 00:05:26 +0800

    added initial code for batch 4 program 3

--- programs_batch_4/display_highest_number.py
+++ programs_batch_4/display_highest_number.py
@@ -1,4 +1,22 @@
 #initialize valid_flag, current_highest_number
+valid_flag = True
+current_highest_number = 0
+
 #create while loop to continue asking user for inputs
-#evaluate whether user_input is higher than current_highest_number
-#print current_highest_number
+while valid_flag:
+    user_input = input("input a number = ")
+
+    #evaluate whether user_input is invalid (if statement + break) and set valid_flag to false if true
+    if user_input.isalpha() or user_input.isspace():
+        valid_flag = False
+        break
+
+    #explicitly convert user_input to integer
+    int(user_input)
+
+    #evaluate whether user_input is higher than current_highest_number
+    if user_input > current_highest_number:
+        current_highest_number = user_input
+
+    #print current_highest_number
+    print(current_highest_number)
