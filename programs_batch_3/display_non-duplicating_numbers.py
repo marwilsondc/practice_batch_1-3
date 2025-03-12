f383b653cd3fa26bb70cf6eeace7da07ba0fcdb8
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 15:44:39 +0800

    added pseudocode for batch 3 program 1

---
+++ programs_batch_3/display_non-duplicating_numbers.py
@@ -0,0 +1,6 @@
+#initialize a number list and display list
+#create a while loop to ask user to input 10 numbers
+#append user inputs to number list and display list
+#evaluate whether user input is already in number list (if statement)
+#pop duplicated inputs from display list
+#display results from display list
