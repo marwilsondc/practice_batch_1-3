c72a9cc02b94dda31e5abeea0e5887845ae9b2ce
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 17:26:24 +0800

    added code for batch 3 program 4

--- programs_batch_3/display_lowest_number.py
+++ programs_batch_3/display_lowest_number.py
@@ -1,5 +1,20 @@
 #initialize valid_flag and number_list
+valid_flag = True
+number_list = list()
+
 #create while loop that continuously asks user for integer inputs
-#evaluate whether user_input is invalid (if statement) and set valid_flag to false if true
-#append user_input to number_list and use sort()
-#display lowest number with list indexing
+while valid_flag: 
+    user_input = input("input a number = ")
+
+    #evaluate whether user_input is invalid (if statement) and set valid_flag to false if true
+    if user_input.isalpha() or user_input.isspace():
+        valid_flag = False
+        break
+
+    #append user_input to number_list and use sort()
+    number_list.append(int(user_input))
+    number_list.sort()
+
+    #display lowest number with list indexing
+    print(number_list[0])
+    
