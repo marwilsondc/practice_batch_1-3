e8bd9d6cce816b90c57a7bebaafaa3d95f208fc4
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 16:16:10 +0800

    added pseudocode for batch 3 program 2

---
+++ programs_batch_3/display_all_numbers_once.py
@@ -0,0 +1,6 @@
+#initialize number_list and display_list
+#create while loop that asks user for 10 integer inputs
+#append user inputs to number_list and display_list
+#evaluate duplicated inputs in number_list (if statement)
+#remove duplicated inputs in display_list until its count turns to 1
+#display results
