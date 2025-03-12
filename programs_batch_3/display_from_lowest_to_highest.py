b59d5887a1e0704d1f26ea30612edfbbabd6d11a
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 17:31:17 +0800

    added pseudocode for batch 3 program 5

---
+++ programs_batch_3/display_from_lowest_to_highest.py
@@ -0,0 +1,5 @@
+#initialize valid_flag and number_list
+#create while loop to continue asking user for inputs
+#evaluate if user_input is valid (if statement + break) and set valid_flag to false if true
+#append user_input to number_list and use sort()
+#display all numbers from lowest to highest
