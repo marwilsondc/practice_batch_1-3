1e97ecd345ef9878cbdab4c2d78b4c03ab96d5a1
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 16:27:54 +0800

    added code for batch 3 program 2

--- programs_batch_3/display_all_numbers_once.py
+++ programs_batch_3/display_all_numbers_once.py
@@ -1,6 +1,21 @@
 #initialize number_list and display_list
+number_list = list()
+display_list = list()
+
 #create while loop that asks user for 10 integer inputs
-#append user inputs to number_list and display_list
-#evaluate duplicated inputs in number_list (if statement)
-#remove duplicated inputs in display_list until its count turns to 1
+while len(number_list) < 10:
+    user_input = int(input("input a number = "))
+
+    #append user inputs to number_list and display_list
+    number_list.append(user_input)
+    display_list.append(user_input)
+
+    #evaluate duplicated inputs in number_list (if statement)
+    if number_list.count(user_input) > 1:
+
+        #remove duplicated inputs in display_list until its count turns to 1
+        while display_list.count(user_input) > 1:
+            display_list.remove(user_input)
+
 #display results
+print(display_list)
