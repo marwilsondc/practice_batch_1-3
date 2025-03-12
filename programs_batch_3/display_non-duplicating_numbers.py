66c109b6979111607923b5fae67862a75e011be6
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 16:06:14 +0800

    added code for batch 3 program 1

--- programs_batch_3/display_non-duplicating_numbers.py
+++ programs_batch_3/display_non-duplicating_numbers.py
@@ -1,6 +1,21 @@
 #initialize a number list and display list
+number_list = list()
+display_list = list()
+
 #create a while loop to ask user to input 10 numbers
-#append user inputs to number list and display list
-#evaluate whether user input is already in number list (if statement)
-#pop duplicated inputs from display list
+while len(number_list) < 10:
+    user_input = int(input("input a number = "))
+
+    #append user inputs to number list and display list
+    number_list.append(user_input)
+    display_list.append(user_input)
+
+    #evaluate whether user input is already in number list (if statement)
+    if number_list.count(user_input) > 1:
+
+        #pop duplicated inputs from display list
+        while user_input in display_list:
+            display_list.remove(user_input)
+
 #display results from display list
+print(display_list)
