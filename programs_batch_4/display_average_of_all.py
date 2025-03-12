953ed258ca29db80e5411015c9f5e67100059581
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-13 01:06:46 +0800

    added initial pseudocode of batch 4 program 5

---
+++ programs_batch_4/display_average_of_all.py
@@ -0,0 +1,6 @@
+#initialize valid_flag, number_list
+#create while loop to keep asking user to input numbers
+#evaluate whether user_input is invalid (if statement + break) and set valid_flag to false if true
+#explicitly convert user_input to integer and store in convert_input
+#append convert_input into number_list
+#print sum of number_list and divide to length of number_list
