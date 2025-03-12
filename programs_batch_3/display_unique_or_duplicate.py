c76ffd86511bfde3188b20fc300f3b7109660664
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 16:46:55 +0800

    uploaded display_unique_or_duplicate.py and added pseudocode

---
+++ programs_batch_3/display_unique_or_duplicate.py
@@ -0,0 +1,6 @@
+#initialize valid_flag and display_list
+#create a while loop to continue asking user for inputs
+#within while loop, append user_input to number_list
+#evaluate user_input if it's unique (if statement) and return "unique" if true
+#evaluate user_input if it's a duplicate (if statement) and return "duplicate" if true
+#evaluate user_input if it's invalid (if statement) and set valid_flag to false if true
