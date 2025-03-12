15cce80263d6a9b34f162ed63ef47a66c883ec78
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 17:34:06 +0800

    added code for batch 3 program 5

--- programs_batch_3/display_from_lowest_to_highest.py
+++ programs_batch_3/display_from_lowest_to_highest.py
@@ -1,5 +1,19 @@
 #initialize valid_flag and number_list
+valid_flag = True
+number_list = list()
+
 #create while loop to continue asking user for inputs
-#evaluate if user_input is valid (if statement + break) and set valid_flag to false if true
-#append user_input to number_list and use sort()
-#display all numbers from lowest to highest
+while valid_flag:
+    user_input = input("input a number = ")
+
+    #evaluate if user_input is valid (if statement + break) and set valid_flag to false if true
+    if user_input.isalpha() or user_input.isspace():
+        valid_flag = False
+        break
+
+    #append user_input to number_list and use sort()
+    number_list.append(int(user_input))
+    number_list.sort()
+
+    #display all numbers from lowest to highest
+    print(number_list)
