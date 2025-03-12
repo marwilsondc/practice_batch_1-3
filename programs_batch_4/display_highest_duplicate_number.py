8f843d48800646ce702365e2d669dcee00e2b64d
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-12 23:23:01 +0800

    added pseudocode for batch 4 program 2

---
+++ programs_batch_4/display_highest_duplicate_number.py
@@ -0,0 +1,7 @@
+#initialize valid_flag, number_list, high_duplicate, high_duplicate_count
+#create while loop to keep asking user for inputs
+#evaluate user_input if invalid (if statement + break) and set valid_flag to false if true
+#append user_input to number_list
+#evaluate whether number_list.count(user_input) is higher than high_duplicate_count
+#if true, overwrite the value of high_duplicate_count and high_duplicate
+#print(high_duplicate)
